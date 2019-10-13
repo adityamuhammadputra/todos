0aec865cf8d0b3bc4913820fc3ae51085fec52fa
adityamuhammadputra <adityamuhammadputra@gmail.com>
2019-10-13 23:51:35 +0700

    backend default

--- backend/app/Http/Controllers/TodoController.php
+++ backend/app/Http/Controllers/TodoController.php
@@ -11,13 +11,49 @@ class TodoController extends Controller
 {
     public function index()
     {
-        $todo = Todo::all();
+        $todos = Todo::all();
 
-        $data = [
-            'hai' => Auth::user()->name,
-            'todolist' => $todo,
+        // $data = [
+        //     'hai' => Auth::user()->name,
+        //     'todolist' => $todo,
+        // ];
+
+        return response()->json($todos, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $input = [
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => $request->title,
+            'desc' => 'Ini masih hardcode'
         ];
 
-        return response()->json($data, 200);
+        Todo::create($input);
+
+        return response()->json('Berhasil', 200);
+    }
+
+    public function edit(Todo $todo)
+    {
+        return response()->json($todo, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->save();
+
+        return response()->json('Berhasil', 200);
+    }
+
+    public function destroy($id)
+    {
+        $todo = Todo::find($id);
+        $todo->delete();
+
+        return response()->json('Berhasil', 200);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,10 +18,16 @@ Route::group(['prefix' => 'v1'], function () {
     Route::post('register', 'UserController@register');
     Route::post('login', 'UserController@login');
 
-    Route::group(['middleware' => 'jwt.verify'], function () {
+    // Route::group(['middleware' => 'jwt.verify'], function () {
         Route::get('todos', 'TodoController@index');
+        Route::post('todos', 'TodoController@store');
+        Route::patch('todos/{id}', 'TodoController@update');
+        Route::get('todos/{id}/edit', 'TodoController@edit');
+        Route::delete('todos/{id}', 'TodoController@destroy');
+
+
         Route::get('user', 'UserController@getAuthenticatedUser');
-    });
+    // });
 });
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
